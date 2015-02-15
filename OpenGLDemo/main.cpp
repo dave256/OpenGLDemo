e857886948939b37a9b6726ac30fc860b8e2d51d
Dave Reed <dave256@mac.com>
2015-02-15 18:58:41 -0500

    Initial Commit

---
+++ OpenGLDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by David Reed on 2/15/15.
+//  Copyright (c) 2015 David Reed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
