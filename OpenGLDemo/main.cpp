2cef0fcea63ec1dd3c57777ec9ebe3a10d6f3bea
Dave Reed <dave256@mac.com>
2015-02-18 17:05:06 -0500

    output OpenGL info

--- OpenGLDemo/main.cpp
+++ OpenGLDemo/main.cpp
@@ -15,6 +15,9 @@
 
 #include "OpenGLDemo.h"
 
+using std::cerr;
+using std::endl;
+
 // must declare as OpenGLBase for Windows
 OpenGLBase *openGLBase;
 
@@ -35,6 +38,15 @@ int main(int argc, char * argv[])
         return -1;
     }
 #endif
+
+    // print out some info about the graphics drivers
+    cerr << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    cerr << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    cerr << "Vendor: " << glGetString(GL_VENDOR) << endl;
+    cerr << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    GLint textureUnits;
+    glGetIntegerv(GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS, &textureUnits);
+    cerr << "Num texture units: " << textureUnits << endl;
 
     OpenGLDemo *scene = new OpenGLDemo;
     openGLBase = scene;
